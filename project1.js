2a85b8d5b22282024bbe79d714e308bd37f8e82f
SagarKapoor <sagarbadal70@gmail.com>
2025-11-14 21:41:28 +0530

    login page project added to github

---
+++ project1.js
@@ -0,0 +1,33 @@
+let body = document.querySelector("body");
+let spacedone = [];
+function degree(){
+    let deg = Math.floor(Math.random()*360);
+    if(deg % 45 == 0) {
+        console.log(deg);
+        return deg;
+    } else {
+        return degree();
+    }
+}
+function locationLeft(){
+    let left = Math.floor(Math.random()*100);
+    spacedone.push(left);
+    for(let i = 0;i < spacedone.length; i++){
+        if(spacedone[i] == left){
+            return locationLeft();
+        }else{
+            return left + "%";
+        }
+    }
+}
+for(let i = 0; i < 20; i++) {
+    let glowStick = document.createElement("div");
+    glowStick.className = "depth";
+    glowStick.style.position = "fixed";
+    glowStick.style.height = "3000px";
+    glowStick.style.top = "50%";
+    glowStick.style.left = locationLeft();
+    glowStick.style.transform = `translate(-50%,-50%) rotate(${degree()}deg)`;
+    body.prepend(glowStick);
+}
+
